2d441cf15dfb0e7b69cffd6e3641f7ae3a9245f0
u4bi <rootcode10@gmail.com>
2017-12-15 18:43:06 +0900

    Added index.html with react component

---
+++ src/app/app.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class AppComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>U4BI TEST</h1>
+            </div>
+        )
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppComponent from './app/app.component.jsx';
+
+ReactDOM.render(<AppComponent/>, document.getElementById('root'));
