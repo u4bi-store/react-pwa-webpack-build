c1cf19bf2c5e948a3a2f41d4929e7824ddd6c4e5
u4bi <rootcode10@gmail.com>
2017-12-16 14:36:21 +0900

    Updated ESlint rules for prettier
    npm install --save-dev husky lint-staged prettier eslint-config-prettier

--- src/app/app.component.jsx
+++ src/app/app.component.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from './app.component.scss';
-import styles2 from './app.component.css';
+import './app.component.css';
 import logo from '../assets/u4bi.png';
 
-export default class AppComponent extends React.Component {
-    render() {
-        return (
-            <div className={ styles.wrapper }>
-                <img src={ logo } />
-                <h1 className={ styles.color}>U4BI TEST</h1>
-            </div>
-        )
-    }
-}
+const AppComponent = () => (
+  <div className={styles.wrapper}>
+    <img src={logo} alt="u4bi logo" />
+    <h1 className={styles.color}>U4BI TEST</h1>
+  </div>
+);
+
+export default AppComponent;
--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,80 @@
-const path        = require('path'),
-	  webpack     = require('webpack'),
-	  HtmlWebpack = require('html-webpack-plugin'),
-	  ScriptExtHtmlWebpack = require('script-ext-html-webpack-plugin'),
-	  ExtractText = require("extract-text-webpack-plugin");
-	  
+const path = require('path'),
+  webpack = require('webpack'),
+  HtmlWebpack = require('html-webpack-plugin'),
+  ScriptExtHtmlWebpack = require('script-ext-html-webpack-plugin'),
+  ExtractText = require('extract-text-webpack-plugin');
 
 module.exports = {
-	entry: {        
-		app: './src/main.js'
-	},
-	output: {
-        filename: '[name].js',
-        path : path.resolve(__dirname, 'dist')
-    },
-    resolve: { extensions: ['.jsx', '.js'] },
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)?$/,
-				exclude: /node_modules/,
-				use: [
-					{ loader: 'babel-loader' },
-					{
-						loader: "eslint-loader",
-						options: {
-							emitWarning: true
-						}
-					}
-				]
-			},
-			{
-				test: /\.(css|scss)$/,
-				exclude: /node_modules/,
-				use: ExtractText.extract({
-					use: [
-						{
-							loader: 'css-loader',
-							options: {
-							  modules: true,
-							  localIdentName: '[name]_[local]_[hash:base64:5]',
-							  sourceMap: true,
-							}
-						},
-						{ 
-							loader: 'sass-loader',
-							options: {
-								sourceMap: true,
-							}
-						}
-					],
-					fallback: 'style-loader'
-				}) 
-			},
-			{
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file-loader?name=assets/[name].[ext]'
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: { extensions: ['.jsx', '.js'] },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true,
+              emitWarning: true
             }
-		]
-	},
-	plugins: [
-		new webpack.optimize.OccurrenceOrderPlugin(),
-		new webpack.optimize.UglifyJsPlugin({
-            parallel: true,
-            output: { comments: false },
-        }),
-		new HtmlWebpack({
-            template: './src/index.html',
-            inject: false
-		}),
-		new ScriptExtHtmlWebpack({
-			defaultAttribute: 'defer'
-		}),
-		new ExtractText({
-			filename: '[name].css',
-			allChunks: true,
-			ignoreOrder: true
-		})
-	]
-}
+          }
+        ]
+      },
+      {
+        test: /\.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ExtractText.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                localIdentName: '[name]_[local]_[hash:base64:5]',
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      parallel: true,
+      output: { comments: false }
+    }),
+    new HtmlWebpack({
+      template: './src/index.html',
+      inject: false
+    }),
+    new ScriptExtHtmlWebpack({
+      defaultAttribute: 'defer'
+    }),
+    new ExtractText({
+      filename: '[name].css',
+      allChunks: true,
+      ignoreOrder: true
+    })
+  ]
+};
